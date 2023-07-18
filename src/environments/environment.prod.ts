09a470db9106347c5d16468d429a2ce9d6051833
NT-08 <116198434+NT-08@users.noreply.github.com>
2023-07-18 15:06:12 -0300

    qualquer

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  appName: 'IonicFrontEnd',
+  appSlogan: 'Devaneios acumulativos',
+  firebase: {
+    apiKey: "AIzaSyD0QyoGR-Lg1XanEF4S42bZiee6l7a2vZo",
+    authDomain: "ionicfrontend-df02c.firebaseapp.com",
+    projectId: "ionicfrontend-df02c",
+    storageBucket: "ionicfrontend-df02c.appspot.com",
+    messagingSenderId: "1057343897397",
+    appId: "1:1057343897397:web:9815f1ed356a590e91567c"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  appName: 'IonicFrontEnd',
+  appSlogan: 'Devaneios acumulativos',
+  signInMethod: 'redirect', // redirect || popup
+  policiesDate: '2023-07-07',
+  firebase: {
+    apiKey: "AIzaSyD0QyoGR-Lg1XanEF4S42bZiee6l7a2vZo",
+    authDomain: "ionicfrontend-df02c.firebaseapp.com",
+    projectId: "ionicfrontend-df02c",
+    storageBucket: "ionicfrontend-df02c.appspot.com",
+    messagingSenderId: "1057343897397",
+    appId: "1:1057343897397:web:9815f1ed356a590e91567c"
+  }
+};
+
